232b98f54053d8cb35ff804415638e396704dfe5
Mykhailo Soroka <mvsoroka@yandex.ru>
2016-09-26 22:24:19 +0300

    Initial commit

---
+++ MVC5/ContosoUniversity.DataAccess/Configuration/SchoolConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+
+namespace ContosoUniversity.DataAccess.Configuration
+{
+    public class SchoolConfiguration : DbConfiguration
+    {
+        public SchoolConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+        }
+    }
+}
