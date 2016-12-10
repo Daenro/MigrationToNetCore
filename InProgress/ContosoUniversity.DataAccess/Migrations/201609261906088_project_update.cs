c8ab43870d21374cb03702625f02653344a0ca15
Mykhailo Soroka <mvsoroka28@gmail.com>
2016-12-10 14:17:30 +0200

    Added inProgress project

---
+++ InProgress/ContosoUniversity.DataAccess/Migrations/201609261906088_project_update.cs
@@ -0,0 +1,17 @@
+namespace ContosoUniversity.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class project_update : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+            
+        }
+    }
+}
